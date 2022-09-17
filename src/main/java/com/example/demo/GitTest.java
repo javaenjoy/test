03edbdbe5fac8133d28155ebb9906c30a2e5eacb
kimsinok <javaenjoy0714@gmail.com>
2022-09-17 15:45:34 +0900

    Create GitTest

---
+++ src/main/java/com/example/demo/GitTest.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git Test!!");
+
+	}
+
+}
